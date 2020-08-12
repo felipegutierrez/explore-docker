ebf09bfa66693bd412b858e982a55a97a6f4c1c5
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-08-12 15:05:22 +0200

    docker-compose example with restart policy

--- visits/README.md
+++ visits/README.md
@@ -1,5 +1,12 @@
 Docker compose example with two containers connected using network.
 ```
 docker-compose up
+docker-compose up -d
+docker ps
+docker-compose down
 ```
 and access [http://localhost:4081/](http://localhost:4081/).
+
+```
+docker-compose up --build
+```
--- visits/index.js
+++ visits/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const redis = require('redis');
 
+// test the docker crash and recover with process
+const process = require('process');
+
 const app = express();
 const client = redis.createClient({
     host: 'redis-server',
@@ -9,6 +12,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    process.exit(0); // test the docker crash and recover with process
     client.get('visits', (err, visits) => {
         res.send('Hi there! Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
